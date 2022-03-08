2487af83482556f61e07428ca87495a6f212ea5d
gitgregoryfuntera <gregory.funtera@gmail.com>
2022-03-08 18:33:12 +0800

    - added new cart item component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import CustomButton from "../custom-button/custom-button.component";
+import { connect } from "react-redux";
 import "./cart-dropdown.styles.scss";
-const CartDropdown = () => {
+import CartItem from "../cart-item/cart-item.component";
+const CartDropdown = ({ cartItems }) => {
   return (
     <div className="cart-dropdown">
-      <div cart-items></div>
+      <div cart-items>
+        {cartItems.map((item) => (
+          <CartItem key={item.id} {...item} />
+        ))}
+      </div>
       <CustomButton>Go To Checkout</CustomButton>
     </div>
   );
 };
 
-export default CartDropdown;
+const mapToStateProps = ({ cart: { cartItems } }) => ({
+  cartItems,
+});
+
+export default connect(mapToStateProps)(CartDropdown);
